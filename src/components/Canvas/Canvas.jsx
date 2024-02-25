61c02528d7b7519b4ba27cbbb3c798ce1fff3a68
Mahdi Hazrati <mahdihazratimailbox@gmail.com>
2024-02-25 23:24:33 +0330

    Just a circle pull up in screen :))

--- src/components/Canvas/Canvas.jsx
+++ src/components/Canvas/Canvas.jsx
@@ -2,20 +2,32 @@ import { useEffect, useRef } from "react"
 
 function Canvas(props) {
     const canvasRef = useRef(null)
-    const draw = (ctx) => {
-        ctx.fillStyle = "#000"
-        ctx.begaiPath()
-        ctx.arc(50, 100, 20, 0, 2 * Math.PI)
+    const draw = (ctx, frameCount) => {
+        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
+        ctx.fillStyle = '#000000'
+        ctx.beginPath()
+        ctx.arc(50, 100, 20 * Math.sin(frameCount * 0.05) ** 2, 0, 2 * Math.PI)
         ctx.fill()
-
-
-
     }
+
     const handleCanvas = () => {
         const c = canvasRef.current
         const context = c.getContext("2d")
 
-        draw(context)
+        let frameCount = 0
+        let animationFrameId
+
+        //Our draw came here
+        const render = () => {
+            frameCount++
+            draw(context, frameCount)
+            animationFrameId = window.requestAnimationFrame(render)
+        }
+        render()
+
+        return () => {
+            window.cancelAnimationFrame(animationFrameId)
+        }
     }
 
     useEffect(() => {
